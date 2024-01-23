637fc7db3886b0d8255c1bc83058852b5fddcdb0
Tevin Poudrier <tdpoudrier@gmail.com>
2024-01-23 15:37:36 -0700

    Implemented functions

--- lib/DM542t/DM542t.cpp
+++ lib/DM542t/DM542t.cpp
@@ -5,18 +5,33 @@
 
 DM542T::DM542T(uint8_t directionPin, uint8_t pulsePin)
 {
-
+    DirectionPin = directionPin;
+    PulsePin = pulsePin;
+    MicrosecondDelay = DEFAULT_DELAY;
+    CurrentDirection = LOW;
 }
 
-//Start timer and initalize outputs
+//Initalize outputs
 void DM542T::begin()
 {
-
+    digitalWrite(PulsePin, HIGH);
+    digitalWrite(DirectionPin, CurrentDirection);
 }
 
-void DM542T::MoveMotor(uint8_t direction)
+void DM542T::StepMotor()
 {
+    if(CurrentDirection != NewDirection)
+    {
+        //direction leads pulse by >5us
+        digitalWrite(DirectionPin, NewDirection);
+        CurrentDirection = NewDirection;
+        delayMicroseconds(6);
+    }
 
+    //pulse width is >2.5us
+    digitalWrite(PulsePin, LOW);
+    delayMicroseconds(5);
+    digitalWrite(PulsePin, HIGH);
 }
 
 void DM542T::SetDelay(uint32_t delay)
@@ -27,4 +42,15 @@ void DM542T::SetDelay(uint32_t delay)
 uint32_t DM542T::GetDelay()
 {
     return MicrosecondDelay;
+}
+
+//Define direction with HIGH or LOW
+void DM542T::SetDirection(uint8_t direction)
+{
+    NewDirection = direction;
+}
+
+uint8_t DM542T::GetDirection()
+{
+    return CurrentDirection;
 }
--- lib/DM542t/DM542t.h
+++ lib/DM542t/DM542t.h
@@ -5,6 +5,8 @@
 
 #include <Arduino.h>
 
+#define DEFAULT_DELAY 500;
+
 class DM542T {
 public:
     //Constructor
@@ -12,16 +14,19 @@ public:
 
     //Function Prototypes
     void begin();
-    void MoveMotor(uint8_t direction);
+    void StepMotor();
     void SetDelay(uint32_t delay);
     uint32_t GetDelay();
+    void SetDirection(uint8_t direction);
+    uint8_t GetDirection();
 
 private:
 
     //Data members
-    uint32_t MicrosecondDelay = 500;
+    uint32_t MicrosecondDelay;
     uint8_t DirectionPin;
     uint8_t PulsePin;
-    IntervalTimer timer;
+    uint8_t CurrentDirection;
+    uint8_t NewDirection;
 };
 
